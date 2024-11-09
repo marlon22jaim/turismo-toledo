229cf39761f2fd9bae546e3c65be55ce742e7173
Marlon Andres Jaimes Rangel <andres_jr19@hotmail.com>
2024-11-09 13:54:39 -0500

    creando componente para los lugares y archivo para subir cada uno de los lugares en JSON

---
+++ src/data/lugares.js
@@ -0,0 +1,287 @@
+export const lugares = [
+    {
+        id: "hotel-la-nona",
+        title: "Hotel Casa de la Nona",
+        heroImage: "/images/places/alojamiento/hotel-la-nona/hero_la_nona.jpg",
+        heroText: "Ambiente familiar y comodidad para todos!!!",
+        description: "...",
+        video: {
+            url: "/images/places/alojamiento/hotel-la-nona/hotel-nona-hero-video.mp4",
+            alt: "Video del hotel",
+        },
+        images: [
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (1).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (5).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (7).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (9).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (20).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (22).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (23).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (24).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (25).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (28).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (29).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (30).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (35).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (36).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (39).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (42).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+            {
+                url: "/images/places/alojamiento/hotel-la-nona/image_la_nona (44).jpg",
+                alt: "foto hotel",
+                type: "image",
+            },
+        ],
+        services: [
+            {
+                icon: "Seguridad",
+                title: "Camaras de seguridad",
+                description:
+                    "El hotel dispone de un sistema de camaras de seguridad para mayor tranquilidad",
+            },
+            {
+                icon: "Internet",
+                title: "Servicio de internet",
+                description:
+                    "El hotel cuenta con conectividad Wifi para que puedas estar siempre conectado",
+            },
+            {
+                icon: "Variedad",
+                title: "Variedad de camas para todos",
+                description:
+                    "El hotel cuenta con hospedaje para camas matrimoniales, individuales o familiares",
+            },
+            {
+                icon: "Parqueadero",
+                title: "Cuenta con parqueadero",
+                description: "El hotel cuenta con ",
+            },
+        ],
+        contacto: {
+            telefono: "+57 3208367588",
+            direccion: "Calle 11 #3ª – 35 Barrio Napoleón Toledo",
+        },
+        ubicacion: {
+            iframe:
+                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.399055901831!2d-72.48391768980939!3d7.3089864926684465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e689b27e17971e7%3A0x9f6b9ef2758fb049!2sHotel%20Casa%20de%20la%20Nona!5e0!3m2!1ses!2sco!4v1731003533420!5m2!1ses!2sco",
+            descripcion: "Ubicado en Toledo, Norte de Santander",
+        },
+        infoPractica: [
+            {
+                title: "Horarios de atención",
+                content: [
+                    "Abierto todos los días de 6:00 AM a 10:00 PM"
+                ],
+            },
+            {
+                title: "Recomendaciones",
+                content: [
+                    "Reservar mínimo un día antes de tomar una habitación",
+                    "Preguntar por disponibilidad de los servicios"
+                ],
+            },
+        ],
+        showSections: {
+            services: true,   // Sección de servicios habilitada
+            videoGallery: true, // Sección de video habilitada
+            gallery: true,    // Sección de galería habilitada
+            map: true,        // Sección del mapa habilitada
+            infoPractica: true, // Sección de información práctica habilitada
+            contacto: true, // Sección de contacto habilitada
+            direccion: true, // Sección de direccion habilitada
+        },
+    },
+    {
+        // HOTEL LA ORQUIDEA
+        id: "hotel-la-orquidea",
+        title: "Hotel La Orquidea",
+        heroImage:
+            "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea_principal.jpg",
+        heroText: "Ambiente familiar y comodidad para todos!!!",
+        description: "...",
+        video: {
+            url: "/images/places/alojamiento/hotel-la-orquidea/video_hotel_la_orquidea.mp4",
+            alt: "Video del hotel la Orquidea",
+        },
+        images: [
+            {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (18).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (20).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (3).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (4).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (6).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (7).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (10).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (11).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (12).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (14).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+              {
+                url: "/images/places/alojamiento/hotel-la-orquidea/hotel_la_orquidea (15).jpg",
+                alt: "foto hotel",
+                type: "image",
+              },
+        ],
+        services: [
+            {
+                icon: "Seguridad",
+                title: "Camaras de seguridad",
+                description:
+                  "El hotel dispone de un sistema de camaras de seguridad para mayor tranquilidad",
+              },
+              {
+                icon: "Internet",
+                title: "Servicio de internet",
+                description:
+                  "El hotel cuenta con conectividad Wifi para que puedas estar siempre conectado",
+              },
+              {
+                icon: "Variedad",
+                title: "Variedad de camas para todos",
+                description:
+                  "El hotel cuenta con hospedaje para camas matrimoniales, individuales o familiares",
+              },
+              {
+                icon: "Parqueadero",
+                title: "Cuenta con parqueadero",
+                description: "El hotel cuenta con ",
+              },
+        ],
+        contacto: {
+            telefono: "+57 3204753194",
+            direccion: "Cra 5 #12 - 54 Centro Toledo Toledo",
+        },
+        ubicacion: {
+            iframe:
+                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.3902618283287!2d-72.48552568980935!3d7.309979092667468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e689b0493ccf2bf%3A0x128266274937e377!2sHotel%20La%20Orquidea!5e0!3m2!1ses!2sco!4v1731004418044!5m2!1ses!2sco",
+            descripcion: "Ubicado en Toledo, Norte de Santander",
+        },
+        infoPractica: [
+            {
+                title: "Horarios de atención",
+                content: [
+                    "Abierto todos los días de 6:00 AM a 10:00 PM"
+                ],
+            },
+            {
+                title: "Recomendaciones",
+                content: [
+                    "Reservar mínimo un día antes de tomar una habitación",
+                    "Preguntar por disponibilidad de los servicios"
+                ],
+            },
+        ],
+
+        showSections: {
+            services: true,   // Sección de servicios habilitada
+            videoGallery: true, // Sección de video habilitada
+            gallery: true,    // Sección de galería habilitada
+            map: true,        // Sección del mapa habilitada
+            infoPractica: true, // Sección de información práctica habilitada
+            contacto: true, // Sección de contacto habilitada
+            direccion: true, // Sección de direccion habilitada
+        },
+    },
+];
